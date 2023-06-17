20f067ff727d9d254a532dfd88a0684334cce299
KillerxDBr <antonioroberto1407@gmail.com>
2023-06-17 03:45:47 -0400

    including script

---
+++ md5Creator.py
@@ -0,0 +1,39 @@
+import hashlib
+import tempfile
+import subprocess
+from pathlib import Path
+import sqlite3
+
+PADRAO = Path.cwd()
+
+
+# git ls-files --other --exclude-standard
+
+with tempfile.TemporaryFile() as tempf:
+    proc = subprocess.Popen(
+        ['git', 'ls-files', '--other', '--exclude-standard'],
+            stdout=tempf, cwd=PADRAO)
+    proc.wait()
+    tempf.seek(0)
+    files = ''.join([chr(x) for x in tempf.read()]).splitlines()
+
+
+cx = sqlite3.connect('md5.db')
+c = cx.cursor()
+lst = []
+for file in files:
+    p = Path(file)
+    c.execute('select * from MD5 where Filename=? AND Fullpath=?',(p.name, str(p)))
+    rst = c.fetchone()
+    if not rst:
+        print('MD5 Missing')
+        lst.append(file)
+    elif not rst[1] == hashlib.md5(open(file,'rb').read()).hexdigest():
+        print('MD5 False')
+        lst.append(file)
+    else:
+        print(f'MD5 True -> {rst[1]}')
+cx.close()
+
+if lst:
+    print(lst)
