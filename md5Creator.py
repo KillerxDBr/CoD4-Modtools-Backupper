7da5b13ef9329b4deccf765f1e9aedd53ae9e059
KillerxDBr <antonioroberto1407@gmail.com>
2023-06-17 04:06:58 -0400

    Script Update

--- md5Creator.py
+++ md5Creator.py
@@ -4,36 +4,38 @@ import subprocess
 from pathlib import Path
 import sqlite3
 
-PADRAO = Path.cwd()
+if __name__ == '__main__':
 
+    PADRAO = Path(__file__).parent.resolve()
 
-# git ls-files --other --exclude-standard
+    # git ls-files --other --exclude-standard
 
-with tempfile.TemporaryFile() as tempf:
-    proc = subprocess.Popen(
-        ['git', 'ls-files', '--other', '--exclude-standard'],
-            stdout=tempf, cwd=PADRAO)
-    proc.wait()
-    tempf.seek(0)
-    files = ''.join([chr(x) for x in tempf.read()]).splitlines()
-
-
-cx = sqlite3.connect('md5.db')
-c = cx.cursor()
-lst = []
-for file in files:
-    p = Path(file)
-    c.execute('select * from MD5 where Filename=? AND Fullpath=?',(p.name, str(p)))
-    rst = c.fetchone()
-    if not rst or not rst[1] == hashlib.md5(open(file,'rb').read()).hexdigest():
-        # print('MD5 Missing')
-        lst.append(file)
-    # else:
-    #     print(f'MD5 True -> {rst[1]}')
-cx.close()
-
-if lst:
-    for file in lst:
+    with tempfile.TemporaryFile() as tempf:
         proc = subprocess.Popen(
-            ['git', 'add', file], cwd=PADRAO)
-    proc.wait()
+            ['git', 'ls-files', '--other', '--exclude-standard'],
+            stdout=tempf, cwd=PADRAO)
+        proc.wait()
+        tempf.seek(0)
+        files = ''.join([chr(x) for x in tempf.read()]).splitlines()
+
+    cx = sqlite3.connect('md5.db')
+    c = cx.cursor()
+    lst = []
+    for file in files:
+        p = Path(file)
+        c.execute('select * from MD5 where Filename=? AND Fullpath=?',
+                  (p.name, str(p)))
+        rst = c.fetchone()
+        if not rst or not rst[1] == hashlib.md5(open(file, 'rb').read()).hexdigest():
+            # print('MD5 Missing')
+            lst.append(file)
+        # else:
+        #     print(f'MD5 True -> {rst[1]}')
+    cx.close()
+
+    if lst:
+        for file in lst:
+            proc = subprocess.Popen(
+                ['git', 'add', file], cwd=PADRAO)
+        proc.wait()
+        print('Use "git commit -a" to commit all modifications')
